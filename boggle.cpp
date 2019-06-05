781de49f85f080bfe5fb6e6c5c66670aef1a6695
Jeremy Rotter <jeremy.rotter@gmail.com>
2019-06-05 19:30:23 -0400

    counting letters in each dictionary word

--- boggle.cpp
+++ boggle.cpp
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int readwords(char words[66000][17]) {
+int readwords(char words[66000][17]) 
+{
   FILE *fp;
   size_t read;
   size_t len;
   char *line = NULL;
+  int index;
   int wordcount = 0;
 
   // Read in the word list
@@ -15,9 +17,10 @@ int readwords(char words[66000][17]) {
     exit(EXIT_FAILURE);
 
   while ((read = getline(&line, &len, fp)) != -1) {
-    //printf("Retrieved line of length %zu:\n", read);
-    //printf("%s", line);
     if (read < 16) {
+      index = (int) strlen(line);
+      if (index >= 0)
+        line[index-1] = '\0';
       strcpy(words[wordcount++],line);
     }
   }
@@ -26,10 +29,36 @@ int readwords(char words[66000][17]) {
   return(wordcount);
 }
 
-int main() {
-  // Word storage
-  char words[66000][17];
-  int wordcount = 0;
+void compute_lettercounts(char words[66000][17], int wordcount, char lettercounts[66000][26])
+{
+  int i,j;
+  char *word;
+  int wordlength;
+
+  for(i=0;i<wordcount;i++)
+  {
+    // Initialize lettercounts for this word to zero
+    for(j=0;j<26;j++)
+      lettercounts[i][j] = '0';
+
+    word = words[i];
+    wordlength = strlen(word);
+    printf("word=%s, ",word);
+    printf("wordlength=%d, ",wordlength);
+    printf("lettercounts=");
+    for(j=0;j<wordlength;j++)
+      lettercounts[i][word[j]-'a']++;
+    for(j=0;j<26;j++)
+      printf("%c",lettercounts[i][j]);
+    printf("\n");
+  }
+}
+
+int main() 
+{
+  char words[66000][17]; // Word storage
+  char lettercounts[66000][26]; // Letter counts per word
+  int wordcount = 0; // Number of words
 
   // Tile information
   char tiles[16];
@@ -43,6 +72,9 @@ int main() {
   // Read in the word list
   wordcount = readwords(words);
   printf("word count = %d\n", wordcount);
+
+  // Count each letter in each word in our word list
+  compute_lettercounts(words,wordcount,lettercounts);
 
   // Read in the tiles
   printf("Input the 16 tiles, left to right, top to bottom\n");
